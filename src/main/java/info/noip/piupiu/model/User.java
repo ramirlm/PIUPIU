85c5952d2248e496e8599bd260594fc8af97aecc
Ugo Castro <ugocastroo@gmail.com>
2013-04-13 16:14:42 -0300

    Inserting hibernate validations on User.

--- src/main/java/info/noip/piupiu/model/User.java
+++ src/main/java/info/noip/piupiu/model/User.java
@@ -7,34 +7,40 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_SEQ")
-	@SequenceGenerator(name = "USER_SEQ", sequenceName = "USER_SEQ")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+	@SequenceGenerator(name = "user_seq", sequenceName = "user_seq")
 	private Integer id;
-	
-	@Column
+
+	@Column(nullable = false, length = 45)
+	@NotEmpty(message = "- Nome é obrigatório.")
+	@Length(min = 10, max = 45, message = "- O nome deve conter entre 10 e 45 caracteres.")
 	private String name;
-	
-	@Column
-	private String login;
-	
-	@Column
+
+	@Column(nullable = false)
+	@NotEmpty(message = "- Senha é obrigatória.")
 	private String password;
-	
-	@Column
+
+	@Transient
+	private String passwordConfirmation;
+
+	@Column(unique = true, nullable = false)
+	@Email(message = "- E-mail inválido.")
+	@NotEmpty(message = "- E-mail é obrigatório.")
 	private String email;
 
 	public Integer getId() {
 		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
 	}
 
 	public String getName() {
@@ -45,20 +51,20 @@ public class User {
 		this.name = name;
 	}
 
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
 	public String getPassword() {
 		return password;
 	}
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getPasswordConfirmation() {
+		return passwordConfirmation;
+	}
+
+	public void setPasswordConfirmation(String passwordConfirmation) {
+		this.passwordConfirmation = passwordConfirmation;
 	}
 
 	public String getEmail() {
@@ -68,7 +74,5 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
-	
+
 }
