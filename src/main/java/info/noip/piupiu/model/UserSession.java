22289c74836806714b4326e142ef1b60ab4b1cb7
Eduardo Hitek <eduardohitek@gmail.com>
2013-04-15 19:05:41 -0300

    Adicionando alguns imports que estavam faltando

--- src/main/java/info/noip/piupiu/model/UserSession.java
+++ src/main/java/info/noip/piupiu/model/UserSession.java
@@ -2,6 +2,8 @@ package info.noip.piupiu.model;
 
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.caelum.vraptor.ioc.SessionScoped;
+import info.noip.piupiu.model.User;
+
 
 @Component
 @SessionScoped
