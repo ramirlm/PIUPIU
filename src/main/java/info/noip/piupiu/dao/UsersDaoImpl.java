523dccb11b1a39621cdab335a8fc6161a4eba585
Uira <uiradias@gmail.com>
2013-04-16 01:03:26 -0300

    Retirando o acesso ao mongo do dao de usuario, estava ali somente para teste

--- src/main/java/info/noip/piupiu/dao/UsersDaoImpl.java
+++ src/main/java/info/noip/piupiu/dao/UsersDaoImpl.java
@@ -1,10 +1,6 @@
 package info.noip.piupiu.dao;
 
-import info.noip.piupiu.infra.MongoTemplate;
 import info.noip.piupiu.model.User;
-import info.noip.piupiu.model.mongo.Peep;
-
-import java.util.Date;
 
 import javax.persistence.NoResultException;
 
@@ -17,28 +13,16 @@ import br.com.caelum.vraptor.ioc.Component;
 public class UsersDaoImpl implements UsersDao {
 
 	private Session session;
-	private MongoTemplate mongoTemplate;
 
-	public UsersDaoImpl(Session session, MongoTemplate mongoTemplate) {
+	public UsersDaoImpl(Session session) {
 		this.session = session;
-		this.mongoTemplate = mongoTemplate;
 	}
 
 	@Override
 	public User save(User user) {
 		session.save(user);
-		saveOnMongo(user);
 		return user;
 	}
-
-	private void saveOnMongo(User user) {
-	    Peep post = new Peep();
-	    post.setAuthor(user.getEmail());
-	    post.setDate(new Date());
-	    post.setText("Estou testando a configuração do mongo");
-	    
-	    mongoTemplate.getMongoOperations().save(post);
-	} 
 	
 	@Override
 	public User login(User user) {
