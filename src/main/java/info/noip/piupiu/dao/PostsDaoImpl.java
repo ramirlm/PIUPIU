a51087ca522c228818e3b6d205becc924a870769
Uira <uiradias@gmail.com>
2013-04-17 20:56:05 -0300

    Consulta de peeps...

--- src/main/java/info/noip/piupiu/dao/PostsDaoImpl.java
+++ src/main/java/info/noip/piupiu/dao/PostsDaoImpl.java
@@ -2,6 +2,13 @@ package info.noip.piupiu.dao;
 
 import info.noip.piupiu.infra.MongoTemplate;
 import info.noip.piupiu.model.mongo.Peep;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Order;
+import org.springframework.data.mongodb.core.query.Query;
+
 import br.com.caelum.vraptor.ioc.Component;
 
 @Component
@@ -17,6 +24,15 @@ public class PostsDaoImpl implements PostsDao{
 	public Peep save(Peep post) {
 		mongoTemplate.getMongoOperations().save(post);
 		return post;
+	}
+	
+	public List<Peep> findByAuthor(String author, Integer skip, Integer limit){
+		Query query = new Query(Criteria.where("author")
+				.is(author));
+		query.sort().on("date", Order.DESCENDING);
+		query.skip(skip == null ? 0 : skip);
+		query.limit(limit == null ? 0 : limit);
+		return mongoTemplate.getMongoOperations().find(query, Peep.class);
 	}
 	
 }
