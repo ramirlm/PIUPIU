42b7bd3ff9f538d1d2add4b31d571abde320946f
Uira <uiradias@gmail.com>
2013-04-17 22:14:57 -0300

    Teste de PeepsController

---
+++ src/test/java/info/noip/piupiu/controller/PeepsControllerTest.java
@@ -0,0 +1,65 @@
+package info.noip.piupiu.controller;
+
+import static org.mockito.Mockito.when;
+import info.noip.piupiu.dao.PostsDao;
+import info.noip.piupiu.model.User;
+import info.noip.piupiu.model.UserSession;
+import info.noip.piupiu.model.mongo.Peep;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import br.com.caelum.vraptor.util.test.MockResult;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PeepsControllerTest {
+
+	@Mock
+	PostsDao postsDao;
+	
+	@Mock
+	UserSession userSession;
+	
+	User user;
+	
+	List<Peep> peeps;
+	
+	@Before
+	public void setUp(){
+		MockitoAnnotations.initMocks(this);
+		
+		user = new User();
+		user.setEmail("teste@gmail.com");
+	}
+	
+	@Test
+	public void shouldListPeeps(){
+		
+		peeps = new ArrayList<Peep>();
+		
+		Peep peep = new Peep();
+		peep.setAuthor("teste@gmail.com");
+		peep.setText("Teste");
+		peep.setDate(new Date());
+		
+		peeps.add(peep);
+		
+		when(userSession.getUser()).thenReturn(user);
+		when(postsDao.findByAuthor("teste@gmail.com", 0, 50)).thenReturn(peeps);
+		
+		MockResult result = new MockResult();
+		PeepsController controller = new PeepsController(postsDao, result, userSession);
+		controller.list(0, 50);
+		Assert.assertTrue(result.used());
+	}
+	
+}
