7519e695633069ed2b570232dc9c28270cb0d51a
Luis Eugenio <luiseugeniomb@gmail.com>
2013-04-21 11:55:17 -0300

    Colocando em um arquivo so os js dos peepes

--- src/main/webapp/js/peeps.js
+++ src/main/webapp/js/peeps.js
@@ -1,35 +1,41 @@
 function peep(){
-    	var message = $('#new_message').val();
-    	$.ajax({
-		      url: "/piupiu/peeps",
-		      type: "POST",
-		      data: '{ "peep":{ "text":  "' + message + '" }}',
-		      contentType: "application/json",
-		      async: true,
-		      success: function(html){
-		    	  $('#new_message').val('');
-		    	  $('#wall').prepend(html);
-		      },
-		      error: function(data, status, e) {
-		    	  //Show Error Div 
-			  }
-		   });
-    }
+	var message = $('#new_message').val();
+	$.ajax({
+		url: "/piupiu/peeps",
+		type: "POST",
+	    data: '{ "peep":{ "text":  "' + message + '" }}',
+	    contentType: "application/json",
+	    async: true,
+	    success: function(html){
+    	  $('#new_message').val('');
+    	  $('#wall').prepend(html);
+	    },
+	    error: function(data, status, e) {
+    	  //Show Error Div 
+	    }
+	});
+}
     
-    function loadPeeps(author) {
+function loadPeeps() {
     	$.ajax({
-		      url: "/piupiu/peeps/show/"+author,
-		      type: "GET",
-		      async: true,
-		      success: function(html){
-		    	  $('#wall').append(html);
-		      },
-		      error: function(data, status, e) {
-		    	  //Show Error Div 
-			  }
-		   });
-    }
-    function mudarIMG(author){
-    	
-    }
+	      url: "/piupiu/peeps/show",
+	      type: "GET",
+	      async: true,
+	      success: function(html){
+	    	  $('#wall').append(html);
+	      },
+	      error: function(data, status, e) {
+	    	  //Show Error Div 
+		  }
+	   });
+}
     
+function showRepeepDialog(author, peep){
+	$('#new_message').val('RP @' + get_username(author) +': '+ peep);
+	$('html,body').animate({scrollTop: 0},'slow');
+}
+    
+function get_username(email) {
+    var m = email.match('([^@]+)');
+    return m ? m[0] : null;
+}
