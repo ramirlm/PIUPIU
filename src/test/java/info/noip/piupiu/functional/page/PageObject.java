600a4a6e1995d5a7d68c3bfc70b86ad810edf342
Eduardo Henrique <eduardohitek@gmail.com>
2013-05-06 15:05:54 -0300

    Mais alguns testes de integracao

--- src/test/java/info/noip/piupiu/functional/page/PageObject.java
+++ src/test/java/info/noip/piupiu/functional/page/PageObject.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package info.noip.piupiu.functional.page;
 
@@ -12,27 +12,27 @@ import com.thoughtworks.selenium.Selenium;
 public class PageObject {
 
 	private Selenium selenium;
-	
+
 	public PageObject(Selenium selenium){
 		this.selenium = selenium;
 	}
 	public String retornarTitulo(){
 		return selenium.getTitle();
 	}
-	
+
 	public void acessarPagina(){
 		acessarUrl("");
 	}
-	
+
 	public boolean verificarTextoPresente(String textoASerVerificado){
 		return selenium.isTextPresent(textoASerVerificado);
 	}
-	
+
 	public void acessarLink(String link){
 		selenium.click(link);
 		selenium.waitForPageToLoad("30000");
 	}
-	
+
 	/**
 	 * Esse metodo, passando o timeout foi criado para casos em que o retorno do click é uma resposta em ajax,
 	 * ou seja, a página não é recarregada. Logo, o selenium não precisa esperar...
@@ -43,17 +43,22 @@ public class PageObject {
 		selenium.click(link);
 		selenium.waitForPageToLoad(timeout);
 	}
-	
+
 	public void acessarUrl(String url){
 		selenium.open(url);
 		selenium.waitForPageToLoad("30000");
 	}
-	
+
 	public void preencherCampo(String campo, String valor){
 		selenium.type(campo, valor);
 	}
-	
-	
+
+	public void preencherCampoAutoComplete(String campo, String valor){
+		selenium.typeKeys(campo, valor);
+		selenium.setSpeed("2000");
+	}
+
+
 	/**
 	 * Esse valor de 2000ms, foi setado para esperar que o ajax populasse as selectedbox dependentes
 	 * @param campo
@@ -62,5 +67,10 @@ public class PageObject {
 	public void setarValorSelectBox(String campo, String valor){
 		selenium.select(campo, valor);
 		selenium.setSpeed("2000");
+	}
+
+	public void acessarLinkAjax(String link){
+		selenium.click(link);
+		selenium.setSpeed("1000");
 	}
 }
--- src/test/java/info/noip/piupiu/functional/page/PiuPiuPage.java
+++ src/test/java/info/noip/piupiu/functional/page/PiuPiuPage.java
@@ -7,14 +7,24 @@ public class PiuPiuPage extends PageObject {
 	public PiuPiuPage(Selenium selenium) {
 		super(selenium);
 	}
-	
+
 	public void login(String username, String senha){
 		preencherCampo("emailLogin", username);
 		preencherCampo("passwordLogin", senha);
 		acessarLink("css=form[name=\"registerForm\"] > button.btn");
 	}
-	
-	
+
+	public void piar(String texto){
+		preencherCampo("id=new_message", texto);
+		acessarLinkAjax("css=button.btn.btn-info");
+	}
+
+	public void pesquisarUsuario(String usuario){
+		String usuarioType = usuario.substring(0, usuario.length()-1);
+		preencherCampo("id=search", usuarioType);
+		preencherCampoAutoComplete("id=search", usuario.substring(usuario.length()-1));
+		acessarLinkAjax("css=strong");
+	}
 
 
 }
