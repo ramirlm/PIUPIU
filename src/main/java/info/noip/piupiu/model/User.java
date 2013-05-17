5f2c399fa26fb083279476c21ae3bc62ad4029cc
Ramir Mesquita <ramir.mesquita@gmail.com>
2013-05-17 14:08:55 -0300

    Adding Serializable implementation to User

--- src/main/java/info/noip/piupiu/model/User.java
+++ src/main/java/info/noip/piupiu/model/User.java
@@ -1,5 +1,7 @@
 package info.noip.piupiu.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,7 +17,12 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1197161031396565735L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
